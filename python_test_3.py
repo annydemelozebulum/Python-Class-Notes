f4bd75f08afd243c607c3575d461d2ba296035ff
PC2 <annydemelozebulum@gmail.com>
2024-01-24 16:14:04 +0100

    upload

--- python_test_3.py
+++ python_test_3.py
@@ -1,1 +1,44 @@
 print("Hello World! Test three")
+
+#Session 5&6
+
+#Variables Slides
+
+speed = 35
+time = 43
+distance = speed * time
+print(distance)
+
+a = 6
+print (a+2.0)
+
+a/=2
+print(a)
+b = 3
+print(a**b)
+
+
+#Python
+
+name = input("What is your name?")
+age = input("How old are you?")
+age = int(age)
+birth_year = 2023 - age
+
+print(f"OMG {name}, you are {age} years old so you were born in {birth_year}!")
+print("OMG", name, ", you are", age, "years old so you were born in", birth_year)
+
+#If we wrote "seven" as the age, we would obtain an error since we're using an integer version
+#This is not a syntax error, it is an exception that depends on your answer to the code rather than an error in the code itself
+
+
+
+
+
+
+
+
+
+
+
+
