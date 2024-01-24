814c2c888c2e0d2e6b919577826091b659f40fe6
PC2 <annydemelozebulum@gmail.com>
2024-01-24 18:06:16 +0100

    upload

--- iterations.py
+++ iterations.py
@@ -1,1 +1,15 @@
-print("123")
+import random
+lives = 3
+
+while lives:
+    print("You have", lives, "lives left")
+    dice_roll = random.randint(1, 6)
+    if dice_roll == 6:
+        print("You rolled a 6, you win!")
+        break
+    else:
+        print("You rolled a", dice_roll, "try again!")
+        lives-= 1
+
+if lives == 0:
+    print("You lost all your lives, game over!")
