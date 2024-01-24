0439a55f56fdd6bd2f0523597fb0c1e696d75579
PC2 <annydemelozebulum@gmail.com>
2024-01-24 18:15:38 +0100

    upload

--- while .py
+++ while .py
@@ -4,12 +4,13 @@ lives = 3
 while lives:
     print("You have", lives, "lives left")
     dice_roll = random.randint(1, 6)
+
     if dice_roll == 6:
         print("You rolled a 6, you win!")
         break
     else:
         print("You rolled a", dice_roll, "try again!")
         lives-= 1
-
-if lives == 0:
-    print("You lost all your lives, game over!")
+    if lives == 0:
+        print("You lost all your lives, game over!")
+        break
