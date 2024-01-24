61547ac1edbdc2b7e8652fd6a9bd7b67d9e43025
PC2 <annydemelozebulum@gmail.com>
2024-01-24 16:37:31 +0100

    upload

--- exceptions.py
+++ exceptions.py
@@ -1,5 +1,32 @@
+try:
+    age1 = int(input("What is your age player 1?"))
+    age2 = int(input("What is your age player 2?"))
+    res= age1/age2
+except ValueError:
+    print("I am sorry, but that is not a valid number")
+else:
+    print("Player 1 is older tha player 2 by a factor of", res)
 
 #Same process as before
 age = int(input("What is your age?"))
 print(f"Your age is {age}")
 print("Your age is", age)
+
+
+#Catching the exception and assigning a message
+try:
+    age = int(input("What is your age?"))
+except ValueError:
+    print(" I am sorry, but that is not a valid number")
+else:
+    print("Your age is", age)
+
+#Example 2 players, catching errors with more than one value
+try:
+    age1 = int(input("What is your age player 1?"))
+    age2 = int(input("What is your age player 2?"))
+    res= age1/age2
+except ValueError:
+    print("I am sorry, but that is not a valid number")
+else:
+    print("Player 1 is older tha player 2 by a factor of", res)
