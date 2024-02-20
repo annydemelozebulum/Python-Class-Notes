26541115d7928007e23790b53da4ffa048c8f582
PC2 <annydemelozebulum@gmail.com>
2024-02-20 10:34:48 +0100

    upload

---
+++ function.py
@@ -0,0 +1,15 @@
+def greet():
+    """
+    This is a basic function used for teaching
+    It just prints 2 lines of text
+    :return: 7
+
+    """
+    print("Hello, World!")
+    print("Bye World!")
+    return 7
+
+#print("now i will greet you!")
+#greet()
+#print("done greeting")
+
